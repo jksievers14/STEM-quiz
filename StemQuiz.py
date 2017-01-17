952d54a710a02ecd6181d728d147f3a7b04143fe
jksievers14 <jksievers14@gmail.com>
2017-01-17 14:48:52 -0500

    Create StemQuiz.py

---
+++ StemQuiz.py
@@ -0,0 +1,83 @@
+# STEM-quiz
+# This is a basic math quiz that will test your skills
+
+print('This is a test that will show me your skills')
+name = str(input("User, may I ask what your name is? "))
+print('Hello', name + I like that name.')
+
+print('-----------------------------------------------------------------------------------------------------------')
+
+
+# Question 1
+user_name = input('What is 4 x 4?')
+if user_name == "16" :
+    print('That is correct')
+else:
+    print('Sorry that is incorrect')
+
+# Question 2
+user_name = input('What is 8 + 9?')
+if user_name == "17" :
+    print('That is correct')
+else:
+    print('Sorry that is incorrect')
+    
+# Question 3
+user_name = input('If x is a positve integer and when multiplied by 9 gets 45 what is x?')
+if user_name == "5" :
+    print('That is correct')
+else:
+    print('Sorry that is incorrect')
+
+# Question 4
+user_name = input('What is the sum of 3 after it is multiplied by 4?')
+if user_name == "15" :
+    print('That is correct')
+else:
+    print('Sorry that is incorrect')
+
+# Question 5
+user_name = input('What is 9 divided by 3?')
+if user_name == "3" :
+    print('That is correct')
+else:
+    print('Sorry that is incorrect')
+
+# Question 6
+user_name = input('What is 28 over 0?')
+if user_name == "Undefined" :
+    print('That is correct')
+else:
+    print('Sorry that is incorrect')
+
+# Question 7
+user_name = input('What is 7 * 7 + 7?')
+if user_name == "56" :
+    print('That is correct')
+else:
+    print('Sorry that is incorrect')
+
+# Question 8
+user_name = input('What is 9 * 9?')
+if user_name == "81" :
+    print('That is correct')
+else:
+    print('Sorry that is incorrect')
+
+# Question 9
+user_name = input('What class did Ms Maron teach?')
+if user_name == "Chemistry" :
+    print('That is correct')
+else:
+    print('Sorry that is incorrect')
+
+# Question 10
+user_name = input('What is the difference 30 and 3?')
+if user_name == "10" :
+    print('That is correct')
+else:
+    print('Sorry that is incorrect')
+
+
+
+
